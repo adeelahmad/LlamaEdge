8104602d59354080e660f94f2962754fb5cf16b2
Xin Liu <sam@secondstate.io>
2024-01-26 11:24:04 +0800

    feat(llama-api-server): enable `/v1/embeddings`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/backend/ggml.rs
+++ api-server/llama-api-server/src/backend/ggml.rs
@@ -20,6 +20,7 @@ use endpoints::{
     },
     common::{FinishReason, Usage},
     completions::{CompletionChoice, CompletionObject, CompletionRequest},
+    embeddings::{EmbeddingObject, EmbeddingRequest},
     models::{ListModelsResponse, Model},
 };
 use futures::{future, stream};
@@ -64,9 +65,42 @@ pub(crate) async fn models_handler(
     }
 }
 
-pub(crate) async fn _embeddings_handler() -> Result<Response<Body>, hyper::Error> {
-    println!("llama_embeddings_handler not implemented");
-    error::not_implemented()
+pub(crate) async fn embeddings_handler(
+    mut req: Request<Body>,
+) -> Result<Response<Body>, hyper::Error> {
+    // parse request
+    let body_bytes = to_bytes(req.body_mut()).await?;
+    let embedding_request: EmbeddingRequest = match serde_json::from_slice(&body_bytes) {
+        Ok(embedding_request) => embedding_request,
+        Err(e) => {
+            return error::bad_request(format!(
+                "Fail to parse embedding request: {msg}",
+                msg = e.to_string()
+            ));
+        }
+    };
+
+    // ! debug
+    println!("embedding_request: {:?}", embedding_request);
+
+    let fake_embedding_object = EmbeddingObject {
+        index: 0,
+        object: String::from("embedding"),
+        embedding: vec![0.0],
+    };
+
+    // return response
+    let result = Response::builder()
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "*")
+        .header("Access-Control-Allow-Headers", "*")
+        .body(Body::from(
+            serde_json::to_string(&fake_embedding_object).unwrap(),
+        ));
+    match result {
+        Ok(response) => Ok(response),
+        Err(e) => error::internal_server_error(e.to_string()),
+    }
 }
 
 pub(crate) async fn completions_handler(
